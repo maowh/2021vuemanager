893d75c94aec6dd635ed9c0f51ca56afe7510207
毛毛 <1262844895@qq.com>
2022-02-01 20:57:20 +0800

    feat(layout基础架构): 采用layout页面布局，左侧、上侧、右下显示页面，标准的scss和scss用js调用

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-main">AppMain</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.app-main {
+  // 100vh为整个屏幕高度，浏览器可视区域高度，动态变量,减去navbar高度
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  padding: 61px 20px 20px 20px;
+  box-sizing: border-box;
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧menu -->
+    <sidebar
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    ></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部navbar -->
+        <navbar></navbar>
+      </div>
+      <!-- 内容区appmain -->
+      <appMain></appMain>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import AppMain from './components/AppMain.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/sidebar'
+import variables from '@/styles/variables.scss'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  // 动态计算宽度，需要减去左侧菜单栏的宽度
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
