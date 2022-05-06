953277e2192d456db94387b691a4d4a73578bb37
14280666@qq.com <14280666@qq.com>
2022-05-06 16:34:26 +0800

    feat(RBAC权限控制的角色和权限列表): rBAC的角色展示和权限列表的分级展示

--- src/views/permission-list/index.vue
+++ src/views/permission-list/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <div class="">权限列表</div>
+  <div class="">
+    <!-- default-expand-all默认全部展开 -->
+    <!-- tree-props指定子集 -->
+    <el-card>
+      <el-table
+        :data="allPermission"
+        style="width: 100%"
+        border
+        row-key="id"
+        default-expand-all
+        :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+      >
+        <el-table-column
+          prop="permissionName"
+          :label="$t('msg.permission.name')"
+          width="180"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionMark"
+          :label="$t('msg.permission.mark')"
+          width="180"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionDesc"
+          :label="$t('msg.permission.desc')"
+        ></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+import { permissionList } from '@/api/permission'
+import { watchSwitchLang } from '@/utils/i18n'
+
+// 权限分级：
+// 1、一级权限为页面权限
+// permissionMark对应路由名称
+// 2、二级权限为功能权限
+// permissionMark对应功能权限列表
+
+const allPermission = ref([])
+const getPermissionList = async () => {
+  allPermission.value = await permissionList()
+}
+getPermissionList()
+watchSwitchLang(getPermissionList)
 </script>
 
 <style lang="scss" scoped></style>
