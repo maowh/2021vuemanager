2d2450b3c5aab5ce00ccc82682df9f45f5779a21
14280666@qq.com <14280666@qq.com>
2022-05-14 17:27:59 +0800

    feat(为角色指定权限): el-tree用法、获取所有权限、根据用户id获取权限、根据用户id更新权限

--- src/views/role-list/index.vue
+++ src/views/role-list/index.vue
@@ -8,13 +8,26 @@
         </el-table-column>
         <el-table-column :label="$t('msg.role.desc')" prop="describe">
         </el-table-column>
-        <el-table-column :label="$t('msg.role.action')" width="200">
-          <el-button type="primary" size="mini">
+        <!-- #default="{row}"绑定行数据的解析 -->
+        <el-table-column
+          :label="$t('msg.role.action')"
+          width="200"
+          #default="{ row }"
+        >
+          <el-button
+            type="primary"
+            size="mini"
+            @click="onDistributePermissionClick(row)"
+          >
             {{ $t('msg.role.assignPermissions') }}
           </el-button>
         </el-table-column>
       </el-table>
     </el-card>
+    <DistributePermission
+      v-model="visible"
+      :roleId="selectRoleId"
+    ></DistributePermission>
   </div>
 </template>
 
@@ -22,6 +35,7 @@
 import { ref } from 'vue'
 import { roleList } from '@/api/role'
 import { watchSwitchLang } from '@/utils/i18n'
+import DistributePermission from './components/DistributePermission'
 
 const allRoles = ref([])
 const getRoleList = async () => {
@@ -29,6 +43,13 @@ const getRoleList = async () => {
 }
 getRoleList()
 watchSwitchLang(getRoleList)
+
+const visible = ref(false)
+const selectRoleId = ref('')
+const onDistributePermissionClick = (row) => {
+  visible.value = true
+  selectRoleId.value = row.id
+}
 </script>
 
 <style lang="scss" scoped></style>
