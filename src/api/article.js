9041629cf2be44053bdff24410b105486753d49c
14280666@qq.com <14280666@qq.com>
2022-05-23 17:00:54 +0800

    feat(文章排名页面): 获取服务器数据、页码显示处理、相对时间的全局属性、相对时间的国际化

---
+++ src/api/article.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+
+// 获取所有文章
+export const getArticleList = (data) => {
+  return request({
+    url: '/article/list',
+    params: data
+  })
+}
--- src/filters/index.js
+++ src/filters/index.js
@@ -1,4 +1,7 @@
 import dayjs from 'dayjs'
+import rt from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+import store from '@/store'
 
 export const dateFilter = (val, format = 'YYYY-MM-DD') => {
   // 如果val可以转换为数字就转换为数字
@@ -7,6 +10,17 @@ export const dateFilter = (val, format = 'YYYY-MM-DD') => {
   }
   return dayjs(val).format(format)
 }
+dayjs.extend(rt)
+function relativeTime(val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  return dayjs()
+    .locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+    .to(dayjs(val))
+}
+
+// 定义全局属性
 export default (app) => {
-  app.config.globalProperties.$filters = { dateFilter }
+  app.config.globalProperties.$filters = { dateFilter, relativeTime }
 }
